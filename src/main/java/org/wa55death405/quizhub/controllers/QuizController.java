b74f9dbe3c19d938b50073a5ebc7aae382ad027c
wassim <wassimrached404@gmail.com>
2024-05-03 23:23:27 +0100

    made the 3 core controllers endpoints for starting a quiz,submitting attempts and finally finishing the quiz.

--- src/main/java/org/wa55death405/quizhub/controllers/QuizController.java
+++ src/main/java/org/wa55death405/quizhub/controllers/QuizController.java
@@ -3,7 +3,10 @@ package org.wa55death405.quizhub.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.wa55death405.quizhub.dto.QuestionAttemptSubmissionDTO;
 import org.wa55death405.quizhub.services.QuizService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +14,21 @@ import org.wa55death405.quizhub.services.QuizService;
 public class QuizController {
 
     private final QuizService quizService;
+
+    @PostMapping("/{quizId}/start")
+    public ResponseEntity<Integer> startQuizAttempt(@PathVariable Integer quizId) {
+        return ResponseEntity.ok(quizService.startQuizAttempt(quizId));
+    }
+
+    @PostMapping("/{quizAttemptId}/submit")
+    public ResponseEntity<Void> submitQuestionAttempts(@RequestBody List<QuestionAttemptSubmissionDTO> questionAttemptSubmissions, @PathVariable Integer quizAttemptId) {
+        quizService.submitQuestionAttempts(questionAttemptSubmissions, quizAttemptId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{quizAttemptId}/finish")
+    public ResponseEntity<Float> finishQuizAttempt(@PathVariable Integer quizAttemptId) {
+        return ResponseEntity.ok(quizService.finishQuizAttempt(quizAttemptId));
+    }
 
 }
