50feaef6dcc4bc8bcb9b64132ca1cea07501a1bb
wassim <wassimrached404@gmail.com>
2024-05-03 23:18:55 +0100

    generate better fake data with new utility class

--- src/main/java/org/wa55death405/quizhub/QuizHubApplication.java
+++ src/main/java/org/wa55death405/quizhub/QuizHubApplication.java
@@ -4,23 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.wa55death405.quizhub.entities.Quiz;
 import org.wa55death405.quizhub.services.QuizService;
+import org.wa55death405.quizhub.utils.FakeDataUtils;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class QuizHubApplication implements CommandLineRunner{
 
+	private final FakeDataUtils fakeDataUtils;
 	private final QuizService quizService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizHubApplication.class, args);
-
 	}
-
 
 	@Override
 	public void run(String... args) throws Exception {
-		Integer id = quizService.createFakeQuiz();
-		System.out.println("FAKE QUIZ CREATED WITH ID: " + id);
+		Quiz quiz = fakeDataUtils.fillFakeQuizData();
 	}
 }
