54ade5815da84c652a044c4d9d0aa04da5b78419
wassim <wassimrached404@gmail.com>
2024-05-05 11:38:22 +0100

    add handling for EntityNotFoundException

--- src/main/java/org/wa55death405/quizhub/advices/ControllerExceptionHandler.java
+++ src/main/java/org/wa55death405/quizhub/advices/ControllerExceptionHandler.java
@@ -1,15 +1,25 @@
 package org.wa55death405.quizhub.advices;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.wa55death405.quizhub.dto.StandardApiResponse;
+import org.wa55death405.quizhub.enums.StandardApiStatus;
 import org.wa55death405.quizhub.exceptions.InvalidQuestionResponseException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(InvalidQuestionResponseException.class)
-    public ResponseEntity<?> handleInvalidQuestionResponseException(InvalidQuestionResponseException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<StandardApiResponse<Void>> handleInvalidQuestionResponseException(InvalidQuestionResponseException e) {
+        return ResponseEntity.badRequest().body(new StandardApiResponse<>(StandardApiStatus.FAILURE, e.getMessage()));
     }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardApiResponse<Void>> handleEntityNotFoundException(EntityNotFoundException e) {
+        return new ResponseEntity<>(new StandardApiResponse<>(StandardApiStatus.FAILURE, e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
 }
