a130b36968dd7e1fb9d2133f68c8dac4fb66d9c6
wassim <wassimrached404@gmail.com>
2024-05-05 11:39:18 +0100

    remove unnecessary import's

--- src/test/java/org/wa55death405/quizhub/services/QuestionLogicServiceTest.java
+++ src/test/java/org/wa55death405/quizhub/services/QuestionLogicServiceTest.java
@@ -15,7 +15,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 @ExtendWith(MockitoExtension.class)
 class QuestionLogicServiceTest {
