e52b9bc5da90ff3bccacbaa37f1bec5e6423101f
wassim <wassimrached404@gmail.com>
2024-05-05 22:16:58 +0100

    update exception name

---
+++ src/main/java/org/wa55death405/quizhub/exceptions/InputValidationException.java
@@ -0,0 +1,7 @@
+package org.wa55death405.quizhub.exceptions;
+
+public class InputValidationException extends RuntimeException {
+    public InputValidationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/wa55death405/quizhub/exceptions/InvalidQuestionResponseException.java
+++
@@ -1,7 +0,0 @@
-package org.wa55death405.quizhub.exceptions;
-
-public class InvalidQuestionResponseException extends RuntimeException {
-    public InvalidQuestionResponseException(String message) {
-        super(message);
-    }
-}
