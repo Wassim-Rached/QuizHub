636385b2594bd3b0433a76530c0458b661674dc5
wassim <wassimrached404@gmail.com>
2024-05-05 11:45:16 +0100

    used more appropriate response for current api's

--- src/main/java/org/wa55death405/quizhub/controllers/QuizController.java
+++ src/main/java/org/wa55death405/quizhub/controllers/QuizController.java
@@ -1,9 +1,12 @@
 package org.wa55death405.quizhub.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.wa55death405.quizhub.dto.QuestionAttemptSubmissionDTO;
+import org.wa55death405.quizhub.dto.StandardApiResponse;
+import org.wa55death405.quizhub.enums.StandardApiStatus;
 import org.wa55death405.quizhub.services.QuizService;
 
 import java.util.List;
@@ -16,19 +19,19 @@ public class QuizController {
     private final QuizService quizService;
 
     @PostMapping("/{quizId}/start")
-    public ResponseEntity<Integer> startQuizAttempt(@PathVariable Integer quizId) {
-        return ResponseEntity.ok(quizService.startQuizAttempt(quizId));
+    public ResponseEntity<StandardApiResponse<Integer>> startQuizAttempt(@PathVariable Integer quizId) {
+        return new ResponseEntity<>(new StandardApiResponse<>(StandardApiStatus.SUCCESS,"Quiz attempt started successfully",quizService.startQuizAttempt(quizId)), HttpStatus.CREATED);
     }
 
     @PostMapping("/{quizAttemptId}/submit")
-    public ResponseEntity<Void> submitQuestionAttempts(@RequestBody List<QuestionAttemptSubmissionDTO> questionAttemptSubmissions, @PathVariable Integer quizAttemptId) {
+    public ResponseEntity<StandardApiResponse<Void>> submitQuestionAttempts(@RequestBody List<QuestionAttemptSubmissionDTO> questionAttemptSubmissions, @PathVariable Integer quizAttemptId) {
         quizService.submitQuestionAttempts(questionAttemptSubmissions, quizAttemptId);
-        return ResponseEntity.ok().build();
+        return new ResponseEntity<>(new StandardApiResponse<>(StandardApiStatus.SUCCESS,"Question attempts submitted successfully",null), HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/{quizAttemptId}/finish")
-    public ResponseEntity<Float> finishQuizAttempt(@PathVariable Integer quizAttemptId) {
-        return ResponseEntity.ok(quizService.finishQuizAttempt(quizAttemptId));
+    public ResponseEntity<StandardApiResponse<Float>> finishQuizAttempt(@PathVariable Integer quizAttemptId) {
+    return new ResponseEntity<>(new StandardApiResponse<>(StandardApiStatus.SUCCESS,"Quiz attempt finished and processed successfully",quizService.finishQuizAttempt(quizAttemptId)), HttpStatus.OK);
     }
 
 }
