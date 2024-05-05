c113c1cf6f2f58eef68e0f9ad0ff48fdfa5c772a
wassim <wassimrached404@gmail.com>
2024-05-05 22:13:40 +0100

    update enum

--- src/main/java/org/wa55death405/quizhub/enums/QuestionType.java
+++ src/main/java/org/wa55death405/quizhub/enums/QuestionType.java
@@ -1,15 +1,24 @@
 package org.wa55death405.quizhub.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum QuestionType {
-    TRUE_FALSE,
-    SHORT_ANSWER,
-    NUMERIC,
-    FILL_IN_THE_BLANK,
+    TRUE_FALSE("TRUE_FALSE"),
+    SHORT_ANSWER("SHORT_ANSWER"),
+    NUMERIC("NUMERIC"),
+    FILL_IN_THE_BLANK("FILL_IN_THE_BLANK"),
 
-    SINGLE_CHOICE,
-    MULTIPLE_CHOICE,
+    SINGLE_CHOICE("SINGLE_CHOICE"),
+    MULTIPLE_CHOICE("MULTIPLE_CHOICE"),
 
-    OPTION_MATCHING,
+    OPTION_MATCHING("OPTION_MATCHING"),
 
-    OPTION_ORDERING
+    OPTION_ORDERING("OPTION_ORDERING");
+
+    private final String value;
+
+    QuestionType(String value) {
+        this.value = value;
+    }
 }
