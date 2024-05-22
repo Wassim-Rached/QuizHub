968639523f5297cca4d23dcdad2e2843e9523fa0
wassim <wassimrached404@gmail.com>
2024-05-22 09:57:46 +0100

    update the Generation of random Matching Option and fix problem with Multiple Choice Generation

--- src/main/java/org/wa55death405/quizhub/utils/FakeDataGenerator.java
+++ src/main/java/org/wa55death405/quizhub/utils/FakeDataGenerator.java
@@ -5,7 +5,8 @@ import org.wa55death405.quizhub.dto.question.QuestionCreationRequestDTO;
 import org.wa55death405.quizhub.dto.quiz.QuizCreationDTO;
 import org.wa55death405.quizhub.enums.QuestionType;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class FakeDataGenerator {
 
@@ -13,7 +14,7 @@ public class FakeDataGenerator {
 
     public static void fill(QuizCreationDTO quizCreationDTO) {
         quizCreationDTO.setTitle(faker.lorem().sentence());
-        // create all type of questions
+        // Generate one instance for all types of questions
         for (QuestionType questionType : QuestionType.values()) {
             QuestionCreationRequestDTO questionCreationRequestDTO = new QuestionCreationRequestDTO();
             FakeDataGenerator.fill(questionCreationRequestDTO,questionType);
@@ -30,23 +31,57 @@ public class FakeDataGenerator {
                 questionCreationRequestDTO.setAnswer(faker.lorem().word());
                 return;
             case MULTIPLE_CHOICE:
-                for (int i = 0; i < faker.random().nextInt(2, 5); i++) {
+                /*
+                    * At least 2 choices
+                    * At least one choice should be true
+                 */
+                for (int i = 0; i < faker.random().nextInt(1, 4); i++) {
                     questionCreationRequestDTO.getChoices().put(faker.lorem().word(), faker.bool().bool());
                 }
+                questionCreationRequestDTO.getChoices().put(faker.lorem().word(), true);
                 return;
             case SINGLE_CHOICE:
-                // only one will have true
+                /*
+                    * At least 2 choices
+                    * Only one choice should be true
+                 */
                 for (int i = 0; i < faker.random().nextInt(2, 4); i++) {
                     questionCreationRequestDTO.getChoices().put(faker.lorem().word(), false);
                 }
                 questionCreationRequestDTO.getChoices().put(faker.lorem().word(), true);
                 return;
             case OPTION_MATCHING:
-                for (int i = 0; i < faker.random().nextInt(2, 5); i++) {
-                    questionCreationRequestDTO.getOptionMatches().put(faker.lorem().word(), List.of(faker.lorem().word()));
+                /*
+                    * At least 2 'Options'
+                    * At least 2 'Matches'
+                    * Each 'Option' should have at least one match
+                    * A match can be assigned to multiple options or none
+                 */
+                var numberOfOptions = faker.random().nextInt(2, 7);
+                var numberOfMatches = faker.random().nextInt(2,7);
+
+                // Generate the matches that will be used
+                var matches = new ArrayList<String>();
+                for (int i = 0; i < numberOfMatches; i++) {
+                    matches.add(faker.lorem().word());
+                }
+
+                // Assign random amount of 'Matches' to 'Options'
+                for (int i = 0; i < numberOfOptions; i++) {
+                    var option = faker.lorem().word();
+                    var numberOfMatchesToAssign = faker.random().nextInt(1, matches.size());
+
+                    var randomMatchesToAssign = new TreeSet<String>();
+                    for (int j = 0; j < numberOfMatchesToAssign; j++) {
+                        randomMatchesToAssign.add(matches.get(faker.random().nextInt(0, numberOfMatches - 1)));
+                    }
+                    questionCreationRequestDTO.getOptionMatches().put(option, randomMatchesToAssign.stream().toList());
                 }
                 return;
             case OPTION_ORDERING:
+                /*
+                    * At least 2 options
+                 */
                 for (int i = 0; i < faker.random().nextInt(2, 5); i++) {
                     questionCreationRequestDTO.getOrderedOptions().put(i, faker.lorem().word());
                 }
