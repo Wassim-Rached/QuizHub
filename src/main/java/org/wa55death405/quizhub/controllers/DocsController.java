8d8c2a4dd8c6b10e2aac2e38606a1a341d499a55
wassim <wassimrached404@gmail.com>
2024-05-27 21:37:19 +0100

    by using spring build in loading for html in resources/static it can be available in the .jar build

--- src/main/java/org/wa55death405/quizhub/controllers/DocsController.java
+++ src/main/java/org/wa55death405/quizhub/controllers/DocsController.java
@@ -16,17 +16,7 @@ import java.nio.file.Files;
 public class DocsController {
 
     @GetMapping
-    public ResponseEntity<byte[]> getDocs() throws IOException {
-        // Load the docs.html file from the resources directory
-        Resource resource = new ClassPathResource("static/docs/index.html");
-
-        // Read the file content into a byte array
-        byte[] content = Files.readAllBytes(resource.getFile().toPath());
-
-        // Set the appropriate content type for the response
-        MediaType mediaType = MediaType.TEXT_HTML;
-
-        // Return the file content as a ResponseEntity
-        return ResponseEntity.ok().contentType(mediaType).body(content);
+    public String getDocs() throws IOException {
+        return "forward:/docs/index.html";
     }
 }
