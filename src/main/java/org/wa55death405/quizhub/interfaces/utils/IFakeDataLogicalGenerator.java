7397f7e7ad105df9c338eb09b73e90eea2344ef8
wassim <wassimrached404@gmail.com>
2024-05-28 17:39:19 +0100

    improve code readability by adding more comments

--- src/main/java/org/wa55death405/quizhub/interfaces/utils/IFakeDataLogicalGenerator.java
+++ src/main/java/org/wa55death405/quizhub/interfaces/utils/IFakeDataLogicalGenerator.java
@@ -5,14 +5,42 @@ import org.wa55death405.quizhub.entities.*;
 
 import java.util.List;
 
+/*
+    this interface contains all the methods
+    that generate logical fake data for testing
+    it normally contains or calls sensitive and long algorithms,
+    so the generation here is based on logical algorithms
+    NOTE: NO DATABASE OPERATIONS HERE
+ */
 public interface IFakeDataLogicalGenerator {
+
+    /*
+        generates a perfect question attempt submission
+        for a given question with all correct answers
+     */
     QuestionAttemptSubmissionDTO getPerfectScoreQuestionAttemptSubmissionDTO(Question question);
 
+    /*
+        generates a list of perfect question attempt submissions
+        for a given quiz with all correct answers
+     */
     List<QuestionAttemptSubmissionDTO> getPerfectScoreQuestionAttemptSubmissionDTOsForQuiz(Quiz quiz);
 
+    /*
+        generates a random question attempt submission
+        for a given question with random answers
+     */
     QuestionAttemptSubmissionDTO getRandomQuestionAttemptSubmissionDTO(Question question);
 
+    /*
+        generates a list of random question attempt submissions
+        for a given quiz with random answers
+     */
     List<QuestionAttemptSubmissionDTO> getRandomQuestionAttemptSubmissionDTOsForQuiz(Quiz quiz);
 
+    /*
+        generates a quiz attempt for a given quiz
+        that contains a single attempt for each question
+     */
     QuizAttempt generate_QuizAttempt(Quiz quiz);
 }
