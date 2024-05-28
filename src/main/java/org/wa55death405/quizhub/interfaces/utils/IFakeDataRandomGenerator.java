41dbe8d1f3a74955245eff9e903d3bdbfafed5c7
wassim <wassimrached404@gmail.com>
2024-05-28 17:39:19 +0100

    improve code readability by adding more comments

--- src/main/java/org/wa55death405/quizhub/interfaces/utils/IFakeDataRandomGenerator.java
+++ src/main/java/org/wa55death405/quizhub/interfaces/utils/IFakeDataRandomGenerator.java
@@ -4,11 +4,30 @@ import org.wa55death405.quizhub.dto.question.QuestionCreationRequestDTO;
 import org.wa55death405.quizhub.dto.quiz.QuizCreationDTO;
 import org.wa55death405.quizhub.entities.Quiz;
 import org.wa55death405.quizhub.enums.QuestionType;
-
+/*
+    this interface contains all the methods
+    that generate random fake data for testing
+    it normally contains simple and straight forward methods,
+    so the generation here is normally based on random generators
+    like 'faker' or 'random' or 'math.random' etc.
+    NOTE: NO DATABASE OPERATIONS HERE
+ */
 public interface IFakeDataRandomGenerator {
+    /*
+        fills the quiz creation DTO with random data;
+        by default, it will generate one question for each question type
+     */
     void fill(QuizCreationDTO quizCreationDTO);
 
+    /*
+        fills the question creation DTO with random data
+     */
     void fill(QuestionCreationRequestDTO questionCreationRequestDTO, QuestionType questionType);
 
+    /*
+        generates a random quiz with random questions
+        by default, it will generate one question for each question type
+        @use fill(QuizCreationDTO quizCreationDTO) to fill the quiz creation DTO
+     */
     Quiz generate_Quiz();
 }
