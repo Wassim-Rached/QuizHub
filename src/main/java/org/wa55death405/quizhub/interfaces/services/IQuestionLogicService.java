608ba40970da55bf3716720a2396ac707052137c
wassim <wassimrached404@gmail.com>
2024-05-29 20:32:16 +0100

    fix deepsource erros

--- src/main/java/org/wa55death405/quizhub/interfaces/services/IQuestionLogicService.java
+++ src/main/java/org/wa55death405/quizhub/interfaces/services/IQuestionLogicService.java
@@ -8,5 +8,11 @@ import org.wa55death405.quizhub.entities.QuestionAttempt;
     it normally contains sensitive and long algorithms
  */
 public interface IQuestionLogicService {
+    /*
+        * handle a question attempt
+        * by calculating the correctness and all the other needed data
+        * like the correct answers and the wrong answers for each question
+        * @param questionAttempt the question attempt to handle
+     */
     void handleQuestionAttempt(QuestionAttempt questionAttempt);
 }
