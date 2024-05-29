61921f2d02431410cfba67c0a239c3de9c24d92f
wassim <wassimrached404@gmail.com>
2024-05-29 20:18:39 +0100

    clear unused imports

--- src/main/java/org/wa55death405/quizhub/repositories/QuestionRepository.java
+++ src/main/java/org/wa55death405/quizhub/repositories/QuestionRepository.java
@@ -1,8 +1,6 @@
 package org.wa55death405.quizhub.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.wa55death405.quizhub.entities.CorrectOptionMatch;
 import org.wa55death405.quizhub.entities.Question;
 
 import java.util.List;
