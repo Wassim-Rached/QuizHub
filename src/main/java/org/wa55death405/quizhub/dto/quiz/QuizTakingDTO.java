2876b4b7b6ae2f0a2e38844a084d9f0431f9b025
wassim <wassimrached404@gmail.com>
2024-05-29 14:44:43 +0100

    add todo

--- src/main/java/org/wa55death405/quizhub/dto/quiz/QuizTakingDTO.java
+++ src/main/java/org/wa55death405/quizhub/dto/quiz/QuizTakingDTO.java
@@ -3,11 +3,11 @@ package org.wa55death405.quizhub.dto.quiz;
 import lombok.Data;
 import org.wa55death405.quizhub.dto.question.QuestionTakingDTO;
 import org.wa55death405.quizhub.dto.quizAttempt.QuizAttemptTakingDTO;
-import org.wa55death405.quizhub.entities.Question;
 
 import java.util.ArrayList;
 import java.util.List;
 
+// TODO: should be removed no one is using it
 @Data
 public class QuizTakingDTO {
     private Integer id;
