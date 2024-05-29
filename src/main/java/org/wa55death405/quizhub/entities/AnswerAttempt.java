a330261478874e86dd9887534089c84bf0f6a7fa
wassim <wassimrached404@gmail.com>
2024-05-29 15:24:15 +0100

    change 'AnswerAttempt' entity id type from "Integer" to "UUID"

--- src/main/java/org/wa55death405/quizhub/entities/AnswerAttempt.java
+++ src/main/java/org/wa55death405/quizhub/entities/AnswerAttempt.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class AnswerAttempt {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private Boolean isCorrect = null;
     @Column(nullable = false)
