3aea965c33ca641403a0677ca503704a841c059e
wassim <wassimrached404@gmail.com>
2024-05-29 15:23:27 +0100

    change 'AnswerAttempt' entity id type from "Integer" to "UUID"

--- src/main/java/org/wa55death405/quizhub/entities/AnswerAttempt.java
+++ src/main/java/org/wa55death405/quizhub/entities/AnswerAttempt.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Objects;
+import java.util.UUID;
 
 @Entity
 @Data
@@ -14,7 +15,7 @@ import java.util.Objects;
 public class AnswerAttempt {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private UUID id;
     private Boolean isCorrect = null;
     @Column(nullable = false)
     private String answer;
