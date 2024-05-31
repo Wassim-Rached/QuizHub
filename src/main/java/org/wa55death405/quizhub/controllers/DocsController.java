071014c643ab8410abb053b85d277199e51fbe5e
wassim <wassimrached404@gmail.com>
2024-05-31 21:47:13 +0100

    clean file

--- src/main/java/org/wa55death405/quizhub/controllers/DocsController.java
+++ src/main/java/org/wa55death405/quizhub/controllers/DocsController.java
@@ -1,22 +1,16 @@
 package org.wa55death405.quizhub.controllers;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-import java.nio.file.Files;
 
 @Controller
 @RequestMapping("/docs")
 public class DocsController {
 
     @GetMapping
-    public String getDocs() throws IOException {
+    public String getDocs() {
         return "forward:/docs/index.html";
     }
 }
