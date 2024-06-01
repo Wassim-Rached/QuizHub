75079f01a9654fef81c4b4e9b9d1d2c38479497c
wassim <wassimrached404@gmail.com>
2024-06-01 22:31:19 +0100

    implement pagination to quiz search

---
+++ src/main/java/org/wa55death405/quizhub/dto/StandardPageList.java
@@ -0,0 +1,17 @@
+package org.wa55death405.quizhub.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+/*
+    * this class is used as standard response for the paginated list
+    * @param <T> the type of the items in the list
+ */
+@Data
+public class StandardPageList<T>{
+    private Integer currentPage;
+    private Integer currentItemsSize;
+    private Long totalItems;
+    private List<T> items;
+}
--- src/main/java/org/wa55death405/quizhub/repositories/QuizRepository.java
+++ src/main/java/org/wa55death405/quizhub/repositories/QuizRepository.java
@@ -1,11 +1,12 @@
 package org.wa55death405.quizhub.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.wa55death405.quizhub.entities.Quiz;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface QuizRepository extends JpaRepository<Quiz, UUID> {
-    List<Quiz> findAllByTitleContainingIgnoreCase(String title);
+    Page<Quiz> findAllByTitleContainingIgnoreCase(String title, Pageable pageable);
 }
