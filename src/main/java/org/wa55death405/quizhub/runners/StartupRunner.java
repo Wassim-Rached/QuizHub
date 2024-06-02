d549f79fe8edd4fc02e225a5f0901fc1096bb44a
wassim <wassimrached404@gmail.com>
2024-06-02 21:31:40 +0100

    implement the new service ISeedDataLoader in StartupRunner

--- src/main/java/org/wa55death405/quizhub/runners/StartupRunner.java
+++ src/main/java/org/wa55death405/quizhub/runners/StartupRunner.java
@@ -1,21 +1,19 @@
 package org.wa55death405.quizhub.runners;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.wa55death405.quizhub.dto.quiz.QuizCreationDTO;
+import org.wa55death405.quizhub.interfaces.utils.ISeedDataLoader;
 import org.wa55death405.quizhub.repositories.QuizRepository;
 
-import java.io.InputStream;
 import java.util.List;
 
 /*
     * This class is responsible for loading seed data into the database.
-    * It reads the quiz.json file from the resources folder and saves the data into the database.
+    * It reads the quiz_creation.json file from the resources folder and saves the data into the database.
  */
 
 @Component
@@ -24,17 +22,12 @@ import java.util.List;
 public class StartupRunner implements ApplicationRunner {
 
     private final QuizRepository quizRepository;
-    private final ObjectMapper objectMapper;
+    private final ISeedDataLoader seedDataLoader;
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("seed/quiz.json");
-
-        if (inputStream == null) throw new RuntimeException("Failed to load seed data");
-
-        List<QuizCreationDTO> quizCreationDTO = objectMapper.readValue(inputStream, new TypeReference<>(){});
+    public void run(ApplicationArguments args) {
+        List<QuizCreationDTO> quizCreationDTO = seedDataLoader.getQuizCreationDTOs();
 
         quizRepository.saveAll(quizCreationDTO.stream().map(q->q.toEntity(null)).toList());
-
     }
 }
