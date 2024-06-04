793a0e21c277aa07971d030f4902f8db2c89fe80
wassim <wassimrached404@gmail.com>
2024-06-04 23:10:32 +0100

    remove unnecessary empty arrays and objects initialization

--- src/main/java/org/wa55death405/quizhub/dto/questionAttempt/QuestionAttemptResultDTO.java
+++ src/main/java/org/wa55death405/quizhub/dto/questionAttempt/QuestionAttemptResultDTO.java
@@ -19,30 +19,38 @@ public class QuestionAttemptResultDTO {
     private AnswerAttemptResultDTO answerAttempt;
 
     // for MULTIPLE_CHOICE, SINGLE_CHOICE
-    private List<ChoiceAttemptResultDTO> choiceAttempts = new ArrayList<>();
+    private List<ChoiceAttemptResultDTO> choiceAttempts;
 
     // for OPTION_ORDERING
-    private List<OrderedOptionAttemptResultDTO> orderedOptionAttempts = new ArrayList<>();
+    private List<OrderedOptionAttemptResultDTO> orderedOptionAttempts;
 
     // for OPTION_MATCHING
-    private List<OptionMatchAttemptResult> optionMatchAttempts = new ArrayList<>();
+    private List<OptionMatchAttemptResult> optionMatchAttempts;
 
     public QuestionAttemptResultDTO(QuestionAttempt questionAttempt){
 //        this.id = questionAttempt.getId();
         this.correctnessPercentage = questionAttempt.getCorrectnessPercentage();
+
+        // for TRUE_FALSE,SINGLE_CHOICE,SHORT_ANSWER,NUMERIC,FILL_IN_THE_BLANK,
         if (questionAttempt.getAnswerAttempt() != null){
             this.answerAttempt = new AnswerAttemptResultDTO(questionAttempt.getAnswerAttempt());
         }
+
+        // for MULTIPLE_CHOICE, SINGLE_CHOICE
         if(questionAttempt.getChoiceAttempts() != null){
             for(ChoiceAttempt choiceAttempt : questionAttempt.getChoiceAttempts()){
                 choiceAttempts.add(new ChoiceAttemptResultDTO(choiceAttempt));
             }
         }
+
+        // for OPTION_ORDERING
         if(questionAttempt.getOrderedOptionAttempts() != null){
             for(OrderedOptionAttempt orderedOptionAttempt : questionAttempt.getOrderedOptionAttempts()){
                 orderedOptionAttempts.add(new OrderedOptionAttemptResultDTO(orderedOptionAttempt));
             }
         }
+
+        // for OPTION_MATCHING
         if(questionAttempt.getOptionMatchAttempts() != null){
             for(OptionMatchAttempt optionMatchAttempt : questionAttempt.getOptionMatchAttempts()){
                 optionMatchAttempts.add(new OptionMatchAttemptResult(optionMatchAttempt));
