5d381bdad0fb5fdf63a4f0d7d2fd95ce65569e48
wassim <wassimrached404@gmail.com>
2024-06-07 12:05:42 +0100

    implement cors currently for dev use only

--- src/main/java/org/wa55death405/quizhub/configurations/SecurityConfiguration.java
+++ src/main/java/org/wa55death405/quizhub/configurations/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package org.wa55death405.quizhub.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /*
     * This configuration class is responsible for
@@ -21,4 +24,15 @@ public class SecurityConfiguration {
 ////                .formLogin(Customizer.withDefaults());
 //        return http.build();
 //    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080","http://localhost:4200");
+            }
+        };
+    }
 }
